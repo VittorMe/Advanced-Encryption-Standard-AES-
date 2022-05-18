91da37c1c4c824d40706f7c4ab568e73e2a3316f
Vittor Melo <vittordemelo@hotmail.com>
2022-05-18 13:45:43 -0300

    encryptor

--- encryptor.cs
+++ encryptor.cs
@@ -36,11 +36,44 @@ namespace encrypt_decrypt_string
                 throw new Exception(ex.Message, ex.InnerException);
             }
         }
+        static string Decrypt(string textToDecrypt)
+        {
+            try
+            {
+                
+                string ToReturn = "";
+                string publickey = "12345678";
+                string secretkey = "87654321";
+                byte[] privatekeyByte = { };
+                privatekeyByte = System.Text.Encoding.UTF8.GetBytes(secretkey);
+                byte[] publickeybyte = { };
+                publickeybyte = System.Text.Encoding.UTF8.GetBytes(publickey);
+                MemoryStream ms = null;
+                CryptoStream cs = null;
+                byte[] inputbyteArray = new byte[textToDecrypt.Replace(" ", "+").Length];
+                inputbyteArray = Convert.FromBase64String(textToDecrypt.Replace(" ", "+"));
+                using (DESCryptoServiceProvider des = new DESCryptoServiceProvider())
+                {
+                    ms = new MemoryStream();
+                    cs = new CryptoStream(ms, des.CreateDecryptor(publickeybyte, privatekeyByte), CryptoStreamMode.Write);
+                    cs.Write(inputbyteArray, 0, inputbyteArray.Length);
+                    cs.FlushFinalBlock();
+                    Encoding encoding = Encoding.UTF8;
+                    ToReturn = encoding.GetString(ms.ToArray());
+                }
+                return ToReturn;
+            }
+            catch (Exception ae)
+            {
+                throw new Exception(ae.Message, ae.InnerException);
+            }
+        }
         static void Main(string[] args)
         {
             string text= "WaterWorld";
             string encrypted = Encrypt(text);
-            Console.WriteLine(encrypted);
+            string decrypted = Decrypt(encrypted);
+            Console.WriteLine(decrypted);
         }
     }
 }
